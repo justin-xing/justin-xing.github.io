ceac4a0cb54c37ca9af164cae8f68dfbd1499728
Justin Xing <justinxing3@gmail.com>
2023-09-16 11:27:08 -0400

    add tweet to blog page

--- frontend/src/pages/blog/Blog.js
+++ frontend/src/pages/blog/Blog.js
@@ -1,8 +1,12 @@
+import { Tweet } from 'react-tweet';
+
 import classes from './Blog.module.css';
 
 const Blog = () => {
     return (
-      <div className={classes.blogWrapper}>Blog</div>
+      <div className={classes.blogWrapper}>
+        <Tweet id='1703065641488413102'/>
+      </div>
     );
 }
 export default Blog;
