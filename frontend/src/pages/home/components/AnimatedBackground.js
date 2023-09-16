3820a1bb61b454776e9f52bbb79b122999d7bd1f
Justin Xing <justinxing3@gmail.com>
2023-09-15 21:33:23 -0400

    update resume

--- frontend/src/pages/home/components/AnimatedBackground.js
+++ frontend/src/pages/home/components/AnimatedBackground.js
@@ -15,16 +15,16 @@ const AnimatedBackground = () => {
             () => {
                 setIndex((prevIndex) => prevIndex === 999 || prevIndex === 3 ? 0 : prevIndex + 1)
             },
-            6000
+            5000
         );
     }, [index])
 
     return (
         <div className={classes.wrapper}>
-            <img className={`${classes.imageOne} ${index === 999 || index === 0 || index === 3 ? classes.currentImage : ''}`} src={waterloo} key={0}/>
-            <img className={`${classes.imageTwo} ${index === 1 || index === 0 ? classes.currentImage : ''}`} src={busan} key={1}/>
-            <img className={`${classes.imageThree} ${index === 2 || index === 1 ? classes.currentImage : ''}`} src={jeju} key={2}/>
-            <img className={`${classes.imageFour} ${index === 3 || index === 2 ? classes.currentImage : ''}`} src={niagara} key={3}/>
+            <img alt='' className={`${classes.imageOne} ${index === 999 || index === 0 || index === 3 ? classes.currentImage : ''}`} src={waterloo} key={0}/>
+            <img alt='' className={`${classes.imageTwo} ${index === 1 || index === 0 ? classes.currentImage : ''}`} src={busan} key={1}/>
+            <img alt='' className={`${classes.imageThree} ${index === 2 || index === 1 ? classes.currentImage : ''}`} src={jeju} key={2}/>
+            <img alt='' className={`${classes.imageFour} ${index === 3 || index === 2 ? classes.currentImage : ''}`} src={niagara} key={3}/>
         </div>
     )
 }
