b7363f77bc8cd310b6e879deebfb03e7570da0a9
justin-xing <justinxing3@gmail.com>
2025-08-29 00:51:46 -0400

    Update home images to nicer ones

--- frontend/src/pages/home/components/AnimatedBackground.js
+++ frontend/src/pages/home/components/AnimatedBackground.js
@@ -3,9 +3,9 @@ import { useState, useEffect } from 'react';
 import classes from './AnimatedBackground.module.css';
 
 import plane from '../../../assets/home-images/plane.jpg';
-import busan from '../../../assets/home-images/busan.jpg';
+import california from '../../../assets/home-images/california.jpg';
 import jeju from '../../../assets/home-images/jeju.jpg';
-import niagara from '../../../assets/home-images/niagara.jpg';
+import japan from '../../../assets/home-images/japan.jpg';
 
 const AnimatedBackground = () => {
     const [index, setIndex] = useState(999);
@@ -23,8 +23,8 @@ const AnimatedBackground = () => {
         <div className={classes.wrapper}>
             <img alt='' className={`${classes.imageOne} ${index === 999 || index === 0 || index === 3 ? classes.currentImage : ''}`} src={jeju} key={0}/>
             <img alt='' className={`${classes.imageTwo} ${index === 1 || index === 0 ? classes.currentImage : ''}`} src={plane} key={1}/>
-            <img alt='' className={`${classes.imageThree} ${index === 2 || index === 1 ? classes.currentImage : ''}`} src={niagara} key={2}/>
-            <img alt='' className={`${classes.imageFour} ${index === 3 || index === 2 ? classes.currentImage : ''}`} src={busan} key={3}/>
+            <img alt='' className={`${classes.imageThree} ${index === 2 || index === 1 ? classes.currentImage : ''}`} src={japan} key={2}/>
+            <img alt='' className={`${classes.imageFour} ${index === 3 || index === 2 ? classes.currentImage : ''}`} src={california} key={3}/>
         </div>
     )
 }
