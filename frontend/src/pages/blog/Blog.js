ea52d702fc268251ba4440e33295420565ae1ad3
justin-xing <justinxing3@gmail.com>
2025-08-29 12:03:54 -0400

    Add tweet to blog page

--- frontend/src/pages/blog/Blog.js
+++ frontend/src/pages/blog/Blog.js
@@ -5,7 +5,7 @@ import classes from './Blog.module.css';
 const Blog = () => {
     return (
       <div className={classes.blogWrapper}>
-        <Tweet id='1703065641488413102'/>
+        <Tweet id='1273161294854701058'/>
       </div>
     );
 }
